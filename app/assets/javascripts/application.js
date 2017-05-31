e4aa2008d8ea2275e621331d055b0ddfe6a83ada
黄永飞 <h739578083@gmail.com>
2017-05-31 21:12:56 +0800

    add a product image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/alert
 //= require bootstrap/dropdown
-//= require bootstrap-sprockets
 //= require_tree .
