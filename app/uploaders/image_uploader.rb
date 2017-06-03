1103b3c2d6440d26b7da535d413d144a7dd79300
黄永飞 <h739578083@gmail.com>
2017-06-03 18:53:50 +0800

    implement product show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,6 +25,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [400,400]
   end
 
+  version :origin do
+    process resize_to_fill: [720,540]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
