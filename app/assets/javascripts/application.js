f8b6e2bb855b6a6c1f9150d8decf343c5669ecfb
黄永飞 <h739578083@gmail.com>
2017-06-09 23:31:41 +0800

    implement navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/alert
+//= require bootstrap/collapse
 //= require bootstrap/dropdown
 //= require bootstrap/modal
 //= require_tree .
 
+$('#myCollapsible').collapse({
+  toggle: false
+})
 
 (function ($) {
    $.fn.imagezoom = function (options) {
