6de7a4910fba8b6cb95ea6b7ed66b1cde31e5d1b
黄永飞 <h739578083@gmail.com>
2017-06-09 19:43:40 +0800

    implement fangdajing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -124,22 +124,9 @@
 
 
  // 预览图选择
- $(document).on('mouseover', '.intro-preview-item', function () {
-   var src = $(this).find('img').attr('src')
-   $('.intro-bigPic img').attr('src', src)
-     $('#thumb-lens').css('background-image', 'url(' + src + ')')
+ $(document).on('mouseover', '.intro-preview-item', function() {
+   $(this).find('img').attr('src')
+   $('.intro-bigPic img').attr('src', $(this).find('img').attr('src'))
+   $('.intro-bigPic img').attr('rel', $(this).find('img').attr('rel'))
    $(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
-   m.attach({
-         thumb: '#thumb',
-         large: src,
-         largeWrapper: 'preview'
-   })
- })
-
- $('.intro-preview-activeItem').trigger('mouseover')
- $(document).on('mouseover', '.magnifier-thumb-wrapper', function (e) {
-     $('#preview').css('visibility', 'visible')
- })
- $(document).on('mouseout', '.magnifier-thumb-wrapper', function (e) {
-     $('#preview').css('visibility', 'hidden')
  })
